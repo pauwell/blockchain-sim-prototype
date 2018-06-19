e1e39f984d2a4b64217cfda762adc6bc79b6d1a6
Paul Bernitz <paulbernitz@web.de>
2018-06-19 21:45:17 +0200

    Basic address creation with passphrase. Todo: example.

--- src/components/Wallet.js
+++ src/components/Wallet.js
@@ -7,7 +7,67 @@ export default {
   name: 'Wallet',
   data(){
     return {
-      generateKeyPair: false
+      activeContent: 'welcome',
+      encryptionBits: 1024,
+      passPhrase: '',
+      publicKey: '',
+    }
+  },
+  methods: {
+    generateKeys(){
+      
+      let rsaKey = cryptico.generateRSAKey(this.passPhrase, this.encryptionBits);
+      this.publicKey = cryptico.publicKeyString(rsaKey);
+
+
+
+
+
+
+      /*console.log("Matt's passphrase: " + this.passPhrase);
+      console.log("Bit length: " + this.encryptionBits);
+      
+      var MattsRSAkey = cryptico.generateRSAKey(this.passPhrase, this.encryptionBits);
+      var MattsPublicKeyString = cryptico.publicKeyString(MattsRSAkey);       
+      
+      console.log("Matt's public key string:");
+      console.log(MattsPublicKeyString);
+      
+      var PlainText = "Matt, I need you to help me with my Starcraft strategy.";
+      
+      console.log("Sam's message: " + PlainText);
+      
+      var EncryptionResult = cryptico.encrypt(PlainText, MattsPublicKeyString);
+      
+      console.log("The encrypted message:");
+      console.log(EncryptionResult.cipher);        
+      
+      var DecryptionResult = cryptico.decrypt(EncryptionResult.cipher, MattsRSAkey);
+      
+      console.log("The decrypted message:");
+      console.log(DecryptionResult.plaintext);        
+      console.log("DecryptionResult.signature: " + DecryptionResult.signature);
+      this.publicKey = MattsPublicKeyString;*/
+    },
+    pickPassPhrase(){
+      if(this.passPhrase.length >= 10){
+        this.activeContent = 'keygen';
+      }else{
+        alert('Please pick a phrase with at least 10 digits.');
+      }
+    },
+    pickAddress(){
+      if(this.publicKey.length != 0){
+        this.activeContent = 'example';
+      }else{
+        alert('Please press the \'Generate\'-button to generate you address.');
+      }
+    },
+    abort(){
+      // Reset all member.
+      this.passPhrase = '';
+      this.publicKey = '';
+      this.activeContent = 'welcome';
     }
   },
   components: {
@@ -18,19 +78,45 @@ export default {
       <div class="material-card">
         <h1>Wallet</h1>
       </div>
-      <section class="material-card">
+      <div class="material-card" v-show="activeContent !== 'welcome'">
+        <ol>
+          <li>Step 1) Passphrase: <i><span>{{passPhrase}}</span></i></li>
+          <li v-if="activeContent !== 'phrase'">Step 2) Generate address: <i><span>{{publicKey}}</span></i></li>
+          <li v-if="activeContent === 'example'">Step 3) Example</li>
+        </ol>
+      </div>
+      
+      <!-- welcome -->
+      <section class="material-card" v-show="activeContent === 'welcome'">
         <h2>Don't have an address yet?</h2>
         <p>Create your wallet and start mining.</p>
-        <material-button @click.native="generateKeyPair=true">Create address</material-button>
-        <template v-if="generateKeyPair">
-          <h2>Generate key-pair</h2>
-          <label>Public key </label><input type="text" readonly>
-          <p></p>
-          <label>Private key </label><input type="text" readonly>
-          <p></p>
-          <material-button>Generate</material-button>
-          <material-button color="white" @click.native="generateKeyPair=false">Cancel</material-button>
-        </template>
+        <material-button @click.native="activeContent='phrase'">Create address</material-button>
+      </section>
+
+      <!-- phrase -->
+      <section class="material-card" v-show="activeContent === 'phrase'">
+        <h2>Pick your passphrase</h2>
+        <label>Passphrase </label><input type="text" v-model="passPhrase">
+        <p></p>
+        <material-button @click.native="pickPassPhrase()">Pick</material-button>
+        <material-button color="white" @click.native="abort()">Abort</material-button>
+      </section>
+      
+      <!-- keygen -->
+      <section class="material-card" v-show="activeContent === 'keygen'">
+        <h2>Generate your address</h2>
+        <textarea v-model="publicKey" readonly></textarea>
+        <p></p>
+        <material-button @click.native="generateKeys()">Generate</material-button>
+        <material-button @click.native="pickAddress()">Next</material-button>
+        <material-button color="white" @click.native="abort()">Abort</material-button>
+      </section>
+
+      <!-- example -->
+      <section class="material-card" v-show="activeContent === 'example'">
+        <h2>Example</h2>
+        <p></p>
+        <material-button>wuff!</material-button>
       </section>
     </div>`
 }
