e835fee7591a12a8459596487e9c26a37d70c79c
Paul Bernitz <paulbernitz@web.de>
2018-06-23 05:26:23 +0200

    User registration.

--- php/manage_user.php
+++ php/manage_user.php
@@ -1,6 +1,4 @@
 <?php
-    var_dump($_GET);
-
     /*
         @ TODO
     */
@@ -15,16 +13,16 @@
         fclose($handle);
 
         // Debug.
-        echo "<p>Trying to register with ".$_GET['address']."!</p>";
+        echo "<p><b>Trying to register with ".$_GET['address']."!</b></p>";
         $handle = fopen("../res/address_register.txt", 'r');
         $file = fread($handle, filesize("../res/address_register.txt"));
         fclose($handle);
+        echo "<p><b>Registration successfull!</b></p>";
+        echo "<h2>Content of res/address_register.txt</h2>";
         print_r($file);
 
     } else if($_GET['cmd'] === 'login'){    
         // Login to the wallet t.
         echo "<p>Trying to login with ".$_GET['phrase']."!</p>";
     }
-
-    ente();
 ?>
